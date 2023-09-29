a21653501233cb53cafa2ffde0a50a6be912c074
icarobteles <odevicaroteles@gmail.com>
2023-09-29 07:21:22 -0300

    feat: create HomePage component and styles
    Tests for this component remain to be added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,31 @@
-import { Wrapper } from "./styles";
+import { Filters, SearchForm } from "@/components";
+import { Main, MainContent, MainContentTitle, MainPicture, Spaceship, Wrapper } from "./styles";
+import MainImageJpg from "/images/home-image.jpg";
+import MainImageJpg2x from "/images/home-image@2x.jpg";
+import SpaceshipPng from "/images/spaceship.png";
+import { Logo, LogoSubtitle, LogoTitle } from "@/styles";
 
 export function HomePage() {
-  return <Wrapper>HomePage</Wrapper>;
+  return (
+    <Wrapper>
+      <Logo>
+        <LogoSubtitle>Planet Search</LogoSubtitle>
+        <LogoTitle />
+      </Logo>
+      <Main>
+        <MainPicture>
+          <source media="(min-width: 768px)" srcSet={MainImageJpg2x} />
+          <img src={MainImageJpg} alt="Background Image" />
+        </MainPicture>
+        <MainContent>
+          <MainContentTitle>
+            Discover all the information about Planets of the Star Wars Saga
+          </MainContentTitle>
+          <SearchForm />
+          <Filters />
+        </MainContent>
+        <Spaceship src={SpaceshipPng} alt="Spaceship" />
+      </Main>
+    </Wrapper>
+  );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,90 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  padding: 4.5rem 2.90625rem 3.48rem 2.90625rem;
+
+  grid-row: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2.33rem;
+
+  overflow: hidden;
+
+  @media (min-width: 1024px) {
+    padding: 4.69rem;
+    gap: 3.66rem;
+  }
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  max-width: 18.75rem;
+  background-color: rgba(0, 0, 0, 0.7);
+  border-radius: 0.625rem;
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 1024px) {
+    max-width: 50rem;
+    flex-direction: row;
+  }
+`;
+
+export const MainPicture = styled.picture`
+  > img {
+    width: 100%;
+    height: 11.75rem;
+    border-radius: 0.625rem;
+    object-fit: cover;
+
+    @media (min-width: 1024px) {
+      width: 25rem;
+      height: 100%;
+    }
+  }
+`;
+
+export const MainContent = styled.section`
+  padding: 1.7rem 1.56rem 2rem 1.56rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2.19rem;
+
+  @media (min-width: 1024px) {
+    padding: 5.06rem 3.15rem 5.8rem 3.15rem;
+  }
+`;
+
+export const MainContentTitle = styled.h2`
+  color: #fff;
+  text-align: center;
+  font-family: "Montserrat", sans-serif;
+  font-size: 1.125rem;
+  font-weight: 400;
+
+  @media (min-width: 1024px) {
+    font-size: 1.25rem;
+  }
+`;
+
+export const Spaceship = styled.img`
+  max-width: 20.1875rem;
+  max-height: 14.3125rem;
+  position: absolute;
+  z-index: 10;
+  top: 1.38rem;
+  left: 3.63rem;
+  right: 0;
+
+  @media (min-width: 1024px) {
+    max-width: 28.875rem;
+    max-height: 20.5rem;
+    top: 13.44rem;
+    left: -6.5rem;
+  }
+`;
