f865223c8b6d140e2a8065f23e4869acda0a210c
icarobteles <odevicaroteles@gmail.com>
2023-09-29 07:38:56 -0300

    feat: update PlanetPage component

--- src/pages/Planet/index.tsx
+++ src/pages/Planet/index.tsx
@@ -1,5 +1,38 @@
+import { PlanetCard } from "@/components";
 import { Wrapper } from "./styles";
+import { Link, useParams } from "react-router-dom";
+import { IPlanet } from "@/interfaces";
+
+const PLANET_EXAMPLE: IPlanet = {
+  name: "Tatooine",
+  climate: "arid",
+  terrain: "desert",
+  population: 200000,
+  imageUrl: "https://cryptospro.com.br/planetas/planeta_0000_tatooine.png",
+  residents: [
+    "People 01",
+    "People 02",
+    "People 04",
+    "People 06",
+    "People 07",
+    "People 08",
+    "People 09",
+    "People 11",
+    "People 43",
+    "People 62",
+  ],
+  films: ["Films 1", "Films 3", "Films 4", "Films 5", "Films 6"],
+};
 
 export function PlanetPage() {
-  return <Wrapper>PlanetPage</Wrapper>;
+  const { id } = useParams();
+
+  return (
+    <Wrapper>
+      <PlanetCard {...PLANET_EXAMPLE} />
+      <Link to="/" title="Back">
+        Back
+      </Link>
+    </Wrapper>
+  );
 }
