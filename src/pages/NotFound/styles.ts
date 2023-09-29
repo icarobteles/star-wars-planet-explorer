02794c31c4d67a3b7c6606314bc83e513718cf07
icarobteles <odevicaroteles@gmail.com>
2023-09-29 07:13:01 -0300

    feat: update NotFound page

--- src/pages/NotFound/styles.ts
+++ src/pages/NotFound/styles.ts
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  > h1 {
+    text-align: center;
+    font-size: 3rem;
+    color: #fff;
+  }
+`;
