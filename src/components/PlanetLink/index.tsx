048fca04ece676064cadba46df25ef38769a3465
icarobteles <odevicaroteles@gmail.com>
2023-09-30 21:00:58 -0300

    feat: create the PlanetLink component

---
+++ src/components/PlanetLink/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { Wrapper } from "./styles";
+
+interface PlanetLinkProps {
+  href: string;
+  name: string;
+}
+
+export function PlanetLink({ href, name }: PlanetLinkProps) {
+  return (
+    <Wrapper>
+      <Link to={href} title={name}>
+        <h4> {name}</h4>
+        <i></i>
+      </Link>
+    </Wrapper>
+  );
+}
