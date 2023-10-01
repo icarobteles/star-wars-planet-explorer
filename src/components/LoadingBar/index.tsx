abf5261a0f4dc4c8af230884bd62900897b8b52a
icarobteles <odevicaroteles@gmail.com>
2023-09-30 23:18:40 -0300

    feat: add accessibility in LoadingBar component

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -2,7 +2,8 @@ import { Content, Wrapper } from "./styles";
 
 export function LoadingBar() {
   return (
-    <Wrapper>
+    <Wrapper role="spinner" aria-label="Loading" aria-busy="true">
+      <span className="sr-only">Please wait while more data is being processed...</span>
       <Content></Content>
     </Wrapper>
   );
