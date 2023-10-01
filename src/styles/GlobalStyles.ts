2bb4294f2b4266c9acacaf791f26f01366a0d1c8
icarobteles <odevicaroteles@gmail.com>
2023-09-30 23:22:09 -0300

    feat: add sr-only class in GlobalStyles
    To make content appear only to screen readers and be visually hidden.

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -50,6 +50,19 @@ export const GlobalStyles = createGlobalStyle`
     }
   }
 
+  .sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+}
+
+
   #root {
     width: 100vw;
     height: 100vh;
